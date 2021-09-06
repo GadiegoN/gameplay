ef95aa20ce48ca8447e26f5e1b72e03efd6f4acc
GadiegoN <ngadiego@gmail.com>
2021-09-05 21:42:46 -0300

    :package: Criação da tela de SignIn

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 
-import { View, Text, TextInput } from "react-native"
+import { View, Text, Image } from "react-native"
 
+import IllustrationImg from '../../assets/illustration.png'
+import { ButtonIcon } from "../../components/ButtonIcon";
 import { styles } from './styles'
 
 export function SignIn() {
@@ -10,13 +12,25 @@ export function SignIn() {
 
     return (
         <View style={styles.container}>
-            <Text>Olá mundo!</Text>
-
-            <TextInput 
-                style={styles.input}
-                onChangeText={setText}
+            <Image 
+                source={IllustrationImg} 
+                style={styles.image}
+                resizeMode="stretch"
             />
-            <Text>Você digitou: { text }</Text>
+
+            <View style={styles.content}>
+                <Text style={styles.title}>
+                    Conecte-se {`\n`} e organize suas {`\n`} jogatinas
+                </Text>
+                <Text style={styles.subtitle}>
+                    Crie grupos para jogar seus games {`\n`} favoritos com seus amigos
+                </Text>
+
+                <ButtonIcon
+                    title="Entrar com o discord"
+                    activeOpacity={0.79}
+                />
+            </View>
         </View>
     );
 }
